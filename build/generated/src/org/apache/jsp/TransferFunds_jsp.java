3eb97315e387e9f990e9f731a44c56fe820648bb
John D <John.Delia@nielsen.com>
2017-04-26 23:10:51 -0400

    Add files via upload

--- build/generated/src/org/apache/jsp/TransferFunds_jsp.java
+++ build/generated/src/org/apache/jsp/TransferFunds_jsp.java
@@ -4,14 +4,13 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.jsp.*;
 
-public final class Transaction_jsp extends org.apache.jasper.runtime.HttpJspBase
+public final class TransferFunds_jsp extends org.apache.jasper.runtime.HttpJspBase
     implements org.apache.jasper.runtime.JspSourceDependent {
 
   private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
 
   private static java.util.List<String> _jspx_dependants;
 
-  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_if_test;
   private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_import_url_nobody;
 
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
@@ -21,12 +20,10 @@ public final class Transaction_jsp extends org.apache.jasper.runtime.HttpJspBase
   }
 
   public void _jspInit() {
-    _jspx_tagPool_c_if_test = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
     _jspx_tagPool_c_import_url_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
   }
 
   public void _jspDestroy() {
-    _jspx_tagPool_c_if_test.release();
     _jspx_tagPool_c_import_url_nobody.release();
   }
 
@@ -54,24 +51,10 @@ public final class Transaction_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_out = out;
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
-      out.write("<!--\n");
-      out.write("To change this license header, choose License Headers in Project Properties.\n");
-      out.write("To change this template file, choose Tools | Templates\n");
-      out.write("and open the template in the editor.\n");
-      out.write("-->\n");
-      out.write("<!-- \n");
-      out.write("    Name: John M Delia Jr\n");
-      out.write("    SPC Student ID: 434299\n");
-      out.write("    Course COP 2806 - Java Web Applications\n");
-      out.write("-->\n");
-      out.write("<!--\n");
-      out.write("    Page Assignment for Assignment 1\n");
-      out.write("    1. Create a Transaction.html\n");
-      out.write("    2. Add a Header for the page\n");
-      out.write("-->\n");
       out.write("\n");
       out.write("\n");
       out.write("\n");
+      out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n");
       out.write("<!-- Add the tag library to the page -->\n");
       out.write("\n");
       out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n");
@@ -79,48 +62,35 @@ public final class Transaction_jsp extends org.apache.jasper.runtime.HttpJspBase
       if (_jspx_meth_c_import_0(_jspx_page_context))
         return;
       out.write("\n");
+      out.write("        <h1>Transfer Funds Page</h1>\n");
+      out.write("        \n");
       out.write("        <form action=\"TransactionServlet\" method=\"post\">\n");
-      out.write("        <!-- Create a header for the Transaction page  -->\n");
-      out.write("            <header>\n");
-      out.write("                <h1>Transactions</h1>\n");
-      out.write("                <p>Transfer Funds Page</p>\n");
-      out.write("            </header>\n");
-      out.write("            ");
-      if (_jspx_meth_c_if_0(_jspx_page_context))
-        return;
-      out.write("\n");
-      out.write("                <input type=\"hidden\" name=\"action\" value=\"transferfunds\" >\n");
-      out.write("                <label>First Name:</label>\n");
-      out.write("                <span>");
+      out.write("            <label>First Name:</label>\n");
+      out.write("            <span>");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${user.firstName}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write("</span><br>\n");
-      out.write("                <label>Last Name:</label>\n");
-      out.write("                <span>");
+      out.write("            <label>Last Name:</label>\n");
+      out.write("            <span>");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${user.lastName}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write("</span><br>\n");
-      out.write("                <label>Current Checking Balance:</label>\n");
-      out.write("                <span>");
+      out.write("            <label>Current Checking Balance:</label>\n");
+      out.write("            <span>");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${account.Checking}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write("</span><br>\n");
-      out.write("                <label>Current Savings Balance:</label>\n");
-      out.write("                <span>");
+      out.write("            <label>Current Savings Balance:</label>\n");
+      out.write("            <span>");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${account.Savings}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write("</span><br>\n");
-      out.write("                <label>Transfer From:</label>\n");
-      out.write("                <select name=\"transferfrom\">\n");
-      out.write("                    <option value=\"Checking\">Checking</option>\n");
-      out.write("                    <option value=\"Savings\">Savings</option>\n");
-      out.write("                </select></br>\n");
-      out.write("                <label>Transfer To:</label>\n");
-      out.write("                <select name=\"transferto\">\n");
-      out.write("                    <option value=\"Checking\">Checking</option>\n");
-      out.write("                    <option value=\"Savings\">Savings</option>\n");
-      out.write("                </select></br></br>\n");
-      out.write("                <label for=\"transferamount\">Amount to Transfer ($)</label>\n");
-      out.write("                <input type=\"text\" name=\"transferamount\">\n");
-      out.write("                </br></br>\n");
-      out.write("                <input type=\"submit\" value =\"Transfer\">\n");
-      out.write("            </br>\n");
+      out.write("            \n");
+      out.write("            <label class=\"pad_top\" id=\"lblCheck\" title=\"Checking\" >Transfer From Checking: </label>\n");
+      out.write("            <!--Created the input for the Transfer Checking-->\n");
+      out.write("            <input type=\"text\" id=\"txtCheck\" name=\"Checking\"><br>\n");
+      out.write("            \n");
+      out.write("            <label class=\"pad_top\" id=\"lblSave\" title=\"Savings\" >Transfer From Savings: </label>\n");
+      out.write("            <!--Created the input for the Transfer Savings-->\n");
+      out.write("            <input type=\"text\" id=\"txtSave\" name=\"Savings\"><br>\n");
+      out.write("            \n");
+      out.write("            <input type=\"submit\" value =\"Transfer\">\n");
       out.write("        </form>\n");
       out.write("<!-- Add the footer to the page -->        \n");
       if (_jspx_meth_c_import_1(_jspx_page_context))
@@ -162,36 +132,6 @@ public final class Transaction_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_th_c_import_0.doFinally();
       _jspx_tagPool_c_import_url_nobody.reuse(_jspx_th_c_import_0);
     }
-    return false;
-  }
-
-  private boolean _jspx_meth_c_if_0(PageContext _jspx_page_context)
-          throws Throwable {
-    PageContext pageContext = _jspx_page_context;
-    JspWriter out = _jspx_page_context.getOut();
-    //  c:if
-    org.apache.taglibs.standard.tag.rt.core.IfTag _jspx_th_c_if_0 = (org.apache.taglibs.standard.tag.rt.core.IfTag) _jspx_tagPool_c_if_test.get(org.apache.taglibs.standard.tag.rt.core.IfTag.class);
-    _jspx_th_c_if_0.setPageContext(_jspx_page_context);
-    _jspx_th_c_if_0.setParent(null);
-    _jspx_th_c_if_0.setTest(((java.lang.Boolean) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${message != null}", java.lang.Boolean.class, (PageContext)_jspx_page_context, null)).booleanValue());
-    int _jspx_eval_c_if_0 = _jspx_th_c_if_0.doStartTag();
-    if (_jspx_eval_c_if_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
-      do {
-        out.write("\n");
-        out.write("                <p><i>");
-        out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${message}", java.lang.String.class, (PageContext)_jspx_page_context, null));
-        out.write("</i></p>\n");
-        out.write("            ");
-        int evalDoAfterBody = _jspx_th_c_if_0.doAfterBody();
-        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
-          break;
-      } while (true);
-    }
-    if (_jspx_th_c_if_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
-      _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
-      return true;
-    }
-    _jspx_tagPool_c_if_test.reuse(_jspx_th_c_if_0);
     return false;
   }
 
